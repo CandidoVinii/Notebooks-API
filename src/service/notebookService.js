f226f2533ac0ea1ebec8cce3749924563d903db2
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2023-01-19 14:21:47 -0300

    feat: adicionado preço junto ao notebook e feito a ordenação do notebooks de forma decrescente de acordo com o preço

--- src/service/notebookService.js
+++ src/service/notebookService.js
@@ -9,26 +9,57 @@ const helperFunction = async () => {
 
   // Listo todos os dados vindo do site que tem a classe description, transformo em um array e filtro os nooteboks da lenovo
   const lenovoLaptops = await page.evaluate(() => {
-    const laptops = document.querySelectorAll('.description');
-    return Array.from(laptops)
-      .filter((laptop) => laptop.innerText.includes('Lenovo'))
-      .map((lap) => lap.innerText);
+    const laptops = Array.from(document.querySelectorAll('.thumbnail'))
+      .map(laptop => {
+        const description = laptop.querySelector('.description').innerText;
+        const price = laptop.querySelector('.price').innerText;
+        return { description, price } })
+      .filter(laptop => laptop.description.includes('Lenovo'));
+
+    return laptops;
   });
 
   /* Como existe um padrao de como vem as informações eu inteiro sobre o array criado no função lenovooLaptops
   faço o split na string e atribuo ela a chave correspondente no json */
   const computersObject = {};
   lenovoLaptops.map((note, index) => {
-    const splitStringInfo = note.split(', ');
-    computersObject[index] = {
-      id: `${index + 1}`,  
-      model: splitStringInfo[0],
-      screen: splitStringInfo[1],
-      processor: splitStringInfo[2],
-      memory: splitStringInfo[3],
-      storage: splitStringInfo[4],
-      os: splitStringInfo[5],
-    };
+    const splitStringInfo = note.description.split(', ');
+    if (splitStringInfo.length === 6) {
+      computersObject[index] = {
+        id: `${index + 1}`,  
+        model: splitStringInfo[0],
+        screen: splitStringInfo[1],
+        processor: splitStringInfo[2],
+        memory: splitStringInfo[3],
+        storage: splitStringInfo[4],
+        os: splitStringInfo[5],
+        price: note.price
+      };
+    } else if (splitStringInfo.length === 8) {
+      computersObject[index] = {
+        id: `${index + 1}`,  
+        model: splitStringInfo[0],
+        color: splitStringInfo[1],
+        screen: splitStringInfo[2],
+        processor: splitStringInfo[3],
+        memory: splitStringInfo[4],
+        storage: splitStringInfo[5],
+        video_card: splitStringInfo[6],
+        os: splitStringInfo[7],
+        price: note.price
+      };
+    } else if (splitStringInfo.length === 7) {
+      computersObject[index] = {
+        id: `${index + 1}`,  
+        model: splitStringInfo[0],
+        screen: splitStringInfo[1],
+        processor: splitStringInfo[2],
+        memory: splitStringInfo[3],
+        storage: splitStringInfo[4],
+        os: splitStringInfo[6],
+        price: note.price
+      };
+    }
   });
 
   //aqui eu transformo o objeto criado acima para um array de objetos
@@ -36,7 +67,17 @@ const helperFunction = async () => {
 
   await browser.close();
 
-  return computersWithoutIndex;
+  const orderedPrices = computersWithoutIndex.sort((a, b) => {
+    // Converte o preço de string para float antes de comparar
+    const priceA = parseFloat(a.price);
+    const priceB = parseFloat(b.price);
+    // Compara os preços e retorna -1 se A < B, 0 se A = B e 1 se A > B
+    if (priceA < priceB) return -1;
+    if (priceA > priceB) return 1;
+    return 0;
+  });
+  
+  return orderedPrices;
 };
 
 const getAllNotebooks = async () => {
